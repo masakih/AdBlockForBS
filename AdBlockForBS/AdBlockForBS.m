656ce4ff740846c888628a434295e4c8823e40a3
masakih <masakih@users.sourceforge.jp>
2015-02-27 20:54:18 +0900

    plistから広告ホストを取得しそのホストの時は404ステータスを返すようにした

--- AdBlockForBS/AdBlockForBS.m
+++ AdBlockForBS/AdBlockForBS.m
@@ -11,11 +11,26 @@
 #import "CustomHTTPProtocol.h"
 
 
+static NSArray *sSystemBlockHosts = nil;
+
+
 @interface AdBlockForBS () <CustomHTTPProtocolDelegate>
 @end
 
 @implementation AdBlockForBS
-
++ (void)load
+{
+	static dispatch_once_t onceToken;
+	dispatch_once(&onceToken, ^{
+		NSBundle *bundle = [NSBundle bundleForClass:[self class]];
+		NSURL *url = [bundle URLForResource:@"SystemBlockHost" withExtension:@"plist"];
+		NSArray *array = [[NSArray alloc] initWithContentsOfURL:url];
+		if(!array) {
+			NSLog(@"Can not load SystemBlockHost.plist.");
+		}
+		sSystemBlockHosts = [array copy];
+	});
+}
 - (id)initWithPreferences:(AppDefaults *)prefs
 {
 	self = [super init];
@@ -29,6 +44,25 @@
 {
 	NSURL *url = protocol.request.URL;
 	NSLog(@"Did Finish Loading. URL %@", url);
+}
+
+- (NSURLResponse *)customHTTPProtocol:(CustomHTTPProtocol *)protocol replaceResponse:(NSURLResponse *)response
+{
+	NSString *hostName = response.URL.host;
+	for(NSString *s in sSystemBlockHosts) {
+		if([s hasSuffix:hostName]) {
+			NSHTTPURLResponse *httpResponse = (NSHTTPURLResponse *)response;
+			id headers = httpResponse.allHeaderFields;
+			response = [[NSHTTPURLResponse alloc] initWithURL:response.URL
+												   statusCode:404
+												  HTTPVersion:@"HTTP/1.1"
+												 headerFields:headers];
+			
+			break;
+		}
+	}
+	
+	return response;
 }
 
 
